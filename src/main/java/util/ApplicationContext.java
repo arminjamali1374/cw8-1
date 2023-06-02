fa0c9ebe32b35d340cbbd7088822bb1c6581536b
Pouya Nik <pnima0261@gmail.com>
2023-06-02 08:42:42 +0200

    Create a Project

---
+++ src/main/java/util/ApplicationContext.java
@@ -0,0 +1,18 @@
+package util;
+
+import java.sql.Connection;
+
+public class ApplicationContext {
+   static Connection connection ;
+
+    public static Connection getConnection() {
+       if (connection != null)
+        return connection;
+       else
+       {
+           connection = new ConnectionUtils().getConnection() ;
+           return  connection ;
+       }
+
+    }
+}
