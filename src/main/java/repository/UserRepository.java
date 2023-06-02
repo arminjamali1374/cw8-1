f8c9a578347ce12aabac97057be0124b49f0f2c7
Shahrad <shahradbagheri6@gmail.com>
2023-06-02 10:26:23 +0330

    created methods in UserRepository and created the UserRepositoryImpl class

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package repository;
 
-public class UserRepository {
+import org.example.entity.User;
+
+public interface UserRepository {
+    void save (User user);
+    User load (int userId);
+    void delete (int userId);
+    boolean validUsername(User user);
 }
