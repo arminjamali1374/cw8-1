51598605053aab86cd00dee7c109211dddb6f5ff
Shahrad <shahradbagheri6@gmail.com>
2023-06-02 11:40:30 +0330

    created CategoryRepository

--- src/main/java/repository/CategoryRepository.java
+++ src/main/java/repository/CategoryRepository.java
@@ -1,5 +1,13 @@
 package repository;
 
-public interface CategoryRepository {
+import org.example.entity.Brand;
+import org.example.entity.Category;
 
+import java.sql.SQLException;
+
+public interface CategoryRepository {
+    void save (Category category) throws SQLException;
+    Brand load (int categoryId) throws SQLException;
+    void delete (int categoryId) throws SQLException;
+    boolean existsByName (String categoryName) throws SQLException;
 }
