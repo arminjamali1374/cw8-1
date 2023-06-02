054b883fa3ac0add7e67a202f974b66598f37081
Pouya Nik <pnima0261@gmail.com>
2023-06-02 08:29:11 +0200

    Create a Project

--- src/main/java/org/example/entity/Brand.java
+++ src/main/java/org/example/entity/Brand.java
@@ -1,4 +1,47 @@
 package org.example.entity;
 
 public class Brand {
+    private  int id ;
+    private  String name ;
+    private  String website ;
+    private  String description ;
+
+    public Brand(int id, String name, String website, String description) {
+        this.id = id;
+        this.name = name;
+        this.website = website;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/org/example/entity/Product.java
+++ src/main/java/org/example/entity/Product.java
@@ -1,4 +1,58 @@
 package org.example.entity;
 
 public class Product {
+    private  int id ;
+    private  String name ;
+    private String createData ;
+
+    private  int categoryId ;
+    private  int brandId ;
+
+    public Product(int id, String name, String createData, int categoryId, int brandId) {
+        this.id = id;
+        this.name = name;
+        this.createData = createData;
+        this.categoryId = categoryId;
+        this.brandId = brandId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreateData() {
+        return createData;
+    }
+
+    public void setCreateData(String createData) {
+        this.createData = createData;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public int getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(int brandId) {
+        this.brandId = brandId;
+    }
 }
